c0b0390b5f81dd462bc70ea28d1247201a7b7dfd
Eldos Yksanov <eldosyksanov@gmail.com>
2024-09-16 15:21:40 +0600

    Create Client and Server solution and added new question

---
+++ Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RunClient();
+    }
+
+    static void RunClient()
+    {
+        try
+        {
+            while (ClientWorker()) { }
+            Console.WriteLine("Client stopped!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    
+    static bool ClientWorker()
+    {
+        IPAddress ipAddress = IPAddress.Loopback; // localhost or 127.0.0.1 - наш компьютер
+        IPEndPoint ipep = new IPEndPoint(ipAddress, 8000);
+        using Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        sender.Connect(ipep);
+        Console.WriteLine($"You connected to {sender.RemoteEndPoint}");
+        Console.WriteLine("Enter your message:");
+        string message = Console.ReadLine();
+        byte[] sendingBytes = Encoding.UTF8.GetBytes(message);
+        sender.Send(sendingBytes);
+        byte[] recvbytes = new byte[1024];
+        int bytesReceived = sender.Receive(recvbytes);
+        string response = Encoding.UTF8.GetString(recvbytes, 0, bytesReceived);
+        Console.WriteLine(response);
+        sender.Shutdown(SocketShutdown.Both);
+        sender.Close();
+        return !message.Contains("Stop");
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RunServer();
+    }
+
+    static void RunServer()
+    {
+        try
+        {
+            ServerWorker();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static void ServerWorker()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        IPEndPoint ipep = new IPEndPoint(ipAddress, 8000);
+        using Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        listener.Bind(ipep);
+        listener.Listen(10);
+        while (true)
+        {
+            Console.WriteLine("Waiting for a connection...");
+            using Socket handler = listener.Accept();
+            Console.WriteLine("Client connected!");
+            byte[] receiveBuffer = new byte[1024];
+            int bytesCount = handler.Receive(receiveBuffer);
+            string receiveMessage = Encoding.UTF8.GetString(receiveBuffer, 0, bytesCount);
+            Console.WriteLine(receiveMessage);
+            string response = "".Trim();
+            if (receiveMessage.Contains("Привет"))
+            {
+                response = "Привет с сервера!";
+            }
+            else if (receiveMessage.Contains("Сколько время?"))
+            {
+                response = DateTime.Now.ToLongTimeString();
+            }
+            else if (receiveMessage.Contains("Какое число сегодня?"))
+            {
+                response = DateTime.Today.ToShortDateString();
+            }
+            else if (receiveMessage.Contains("IP адрес сервера?"))
+            {
+                response = ipAddress.ToString();
+            }
+            else if (receiveMessage.Contains("Ты где находишься?"))
+            {
+                response = "Городе Бишкек";
+            }
+            else if (receiveMessage.Contains("Порт"))
+            {
+                response = ipep.Port.ToString();
+            }
+            else if(receiveMessage.Contains("random"))
+            {
+                response = new Random().Next(1, 21).ToString();
+            }
+            else
+            {
+                response = $"Length of your message: {receiveMessage.Length}";
+                response = "Непонятный вопрос!";
+            }
+            byte[] sendBuffer = Encoding.UTF8.GetBytes(response);
+            handler.Send(sendBuffer);
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+            if (receiveMessage.Contains("Stop")) 
+                break;
+        }
+        Console.WriteLine("Server stopped!");
+    }
+}
